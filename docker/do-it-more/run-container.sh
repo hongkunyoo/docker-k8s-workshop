0f6628e454de19d64a1b22832bd6f4630cc09045
hongkunyoo <hongkunyoo@gmail.com>
2020-06-28 16:53:38 +0900

    a

--- docker/do-it-more/run-container.sh
+++ docker/do-it-more/run-container.sh
@@ -1,5 +1,5 @@
 # run mysql
-docker run --rm -e MYSQL_ROOT_PASSWORD=1235 -e MYSQL_USER=ubuntu -e MYSQL_PASSWORD=1234 -p 3306:3306 --name mysql -d mysql
+docker run --rm -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_USER=ubuntu -e MYSQL_PASSWORD=1234 -p 3306:3306 --name mysql -d mysql
 sleep 3
 
 # run flask
